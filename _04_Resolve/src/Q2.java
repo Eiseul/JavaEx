dae7d5bce7349f6312ae914f3c815ff9795b799b
SJF02 <Foaming21@gmail.com>
2025-05-12 17:45:13 +0900

    quest

---
+++ _04_Resolve/src/Q2.java
@@ -0,0 +1,14 @@
+
+public class Q2 {
+    public static void main(String[] args) {
+       
+        int num = 10;
+
+        while( num <= 20 ) {
+            System.out.println(num);
+            num++; }
+        
+        
+        }
+    
+    }
---
+++ _04_Resolve/src/Q3.java
@@ -0,0 +1,13 @@
+public class Q3 {
+    public static void main(String[] args) {
+        int num = 1;    // 숫자 설정 먼저
+    
+
+        while( num <= 10 ) {
+            System.out.println(num);
+            int sum = 0;
+            sum = ( sum + num )
+            num++;
+         }
+    }
+}
