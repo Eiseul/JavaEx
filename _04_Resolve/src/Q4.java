6e9ae2a4403b3d5d66f31f356b77befa0f895679
SJF02 <Foaming21@gmail.com>
2025-05-12 17:45:13 +0900

    quest

---
+++ _04_Resolve/src/Q4.java
@@ -0,0 +1,10 @@
+public class Q4 {
+    public static void main(String[] args) {
+        int num = 1;    // 숫자 설정 먼저
+
+        while( num <= 10 ) {
+            System.out.println(num)
+            num2%==0;
+         }
+    }
+}
