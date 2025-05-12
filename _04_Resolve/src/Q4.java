01ac0b6f2885d9d883f21eeea4c49bee53319497
minchanp123 <minchanp@naver.com>
2025-05-12 21:53:56 +0900

    주석 제거거

--- _04_Resolve/src/Q4.java
+++ _04_Resolve/src/Q4.java
@@ -1,7 +1,7 @@
 public class Q4 {
     public static void main(String[] args) {
         //문제4 while문을 이용해서 1부터 10까지 중에 짝수를 출력하세요
-        int num = 1;    // 숫자 설정 먼저
+        int num = 1;
 
         while( num <= 10 ) {
             System.out.println(num);
