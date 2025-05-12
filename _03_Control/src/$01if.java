98bc9b441d09c1a2e5ea3228b8021f7ba27124ee
SJF02 <Foaming21@gmail.com>
2025-05-12 15:25:13 +0900

    if-else문법 실습습

---
+++ _03_Control/src/$01if.java
@@ -0,0 +1,17 @@
+public class $01if {
+    import class App
+
+    public class App {
+        public static void main(String[] args) throws Exception {
+            scanner sc = new scanner(system.in);
+    
+            System.out.println("점수 입력 >> ");
+            int score = sc.nextInt();
+    
+            if(score >= 90)
+                system.err.println("A학점");
+
+                as.close();
+        }
+    }
+}
