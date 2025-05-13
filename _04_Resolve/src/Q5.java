6c4284f695079bc044b7ec7efe5d0b29c33e3ed5
SJF02 <Foaming21@gmail.com>
2025-05-13 11:54:33 +0900

    문제풀이이

--- _04_Resolve/src/Q5.java
+++ _04_Resolve/src/Q5.java
@@ -1,5 +1,12 @@
 public class Q5 {
     public static void main(String[] args) {
-        // Q5. do~while문을 이용해서 1부터 100까지 출력하세요
+        // Q5. do~while문을 이용해서 1부터 100까지 출력하세요         = 조건을 나중에 검사하는 반복문
+        int num = 1;        // 숫자 선언
+
+        do {
+            System.out.println(num);        //출력
+            num++;                          //증가
+        }
+        while ( num <= 100 );               //조건
     }
 }
---
+++ _04_Resolve/src/Q6.java
@@ -0,0 +1,12 @@
+public class Q6 {
+    public static void main(String[] args) {
+        // Q6. do~while문을 이용해서 10부터 20까지 출력하세요         = 조건을 나중에 검사하는 반복문
+        int num = 10;        // 숫자 선언
+
+        do {
+            System.out.println(num);        //출력
+            num++;                          //증가
+        }
+        while ( num <= 20 );               //조건
+    }
+}
