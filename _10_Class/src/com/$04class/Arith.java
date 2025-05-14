ba183643a34b6f9cf739c175f417084da381d8b3
SJF02 <Foaming21@gmail.com>
2025-05-14 17:58:38 +0900

    연습

---
+++ _10_Class/src/com/$04class/Arith.java
@@ -0,0 +1,16 @@
+package com.$04class;
+
+public class Arith {
+    public int add(int num0, int num1){
+        retrun num0 + num1;
+    }
+    public int sub(int num0, int num1){
+        retrun num0 - num1
+    }
+    public int mul(int num0, int num1){
+        retrun num0 * num1;
+    }
+    public int div(int num0, int num1){
+        retrun num0 / num1;
+    }
+}
---
+++ _연습_/src/basic10.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class basic10 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while (true){
+            int num = sc.nextInt();
+            System.out.print("숫자를 입력하세요 >> ");
+
+            if ( num 
+
+
+            )
+
+        }  
+    }
+}
