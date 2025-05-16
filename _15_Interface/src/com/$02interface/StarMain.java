dc1e88abf2725a9df6d3587111b0d5e2a7a5b73d
SJF02 <Foaming21@gmail.com>
2025-05-16 18:00:53 +0900

    수업

--- _15_Interface/src/com/$02interface/StarMain.java
+++ _15_Interface/src/com/$02interface/StarMain.java
@@ -1,4 +1,5 @@
-package com.$01interface;
+package com.$02interface;
+
 /*
  *          StarUnit
  *   |          |          |
@@ -10,7 +11,12 @@ package com.$01interface;
  * 2) 상속성(inheritance)
  * 3) 다형성(polymorphism)
  */
-
+/*
+ * 새로운 Hydra 클래스는 StarUnit을 상속시킴으로써
+ * 기존의 관리시스템(StarUnit배열)에 합류가 가능하고
+ * 기존 Unit들을 위해 만들어놓은 메서드를 바로 사용할 수 있다.
+ * => 코드양이 줄어든다.
+ */
 public class StarMain {
     public static void allUnitWalk(StarUnit[] units){
         for(StarUnit unit : units)
@@ -28,7 +34,8 @@ public class StarMain {
         StarUnit[] units = new StarUnit[]{
             new Marine(),
             new Scv(),
-            new Zealot()
+            new Zealot(),
+            new Hydra()
         };
 
         allUnitWalk(units);
---
+++ _15_Interface/src/com/$03interface/factory/FordCar.java
@@ -0,0 +1,28 @@
+package com.$03interface.factory;           // 현재 내 패키지 경로
+// 다른 패키지의 선언을 접근할 때 (다른 패키지에 접근하겠다)
+
+import com.$03interface.order.ICar;
+
+public class FordCar implements ICar {
+
+    @Override
+    public void turnOn() {
+
+    }
+
+    @Override
+    public void turnOff() {
+
+    }
+
+    @Override
+    public void accel() {
+
+    }
+
+    @Override
+    public void footBreak() {
+
+    }
+
+}
