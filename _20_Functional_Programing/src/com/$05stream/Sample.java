57b95e8e61c5fbf2ee09a078bd810f9be2f55972
SJF02 <Foaming21@gmail.com>
2025-05-19 18:07:34 +0900

    수업

---
+++ _20_Functional_Programing/src/com/$05stream/Sample.java
@@ -0,0 +1,28 @@
+package com.$05stream;
+
+import java.util.*;
+
+public class Sample {
+    public static void main(String[] args) {
+        int[] data = {5, 6, 4, 2, 3, 1, 1, 2, 2, 4, 8};
+
+        ArrayList<Integer> dataList = new ArrayList<>();
+        for(int i=0; i<data.length; i++) {
+            if(data[i] % 2 == 0) {
+                dataList.add(data[i]);
+            }
+        }
+
+        HashSet<Integer> dataSet = new HashSet<>(dataList);
+
+        ArrayList<Integer> distinctList = new ArrayList<>(dataSet);
+
+        distinctList.sort(Comparator.reverseOrder());
+
+        int[] result = new int[distinctList.size()];
+        for(int i=0; i< distinctList.size(); i++) {
+            result[i] = distinctList.get(i);
+        }
+    }
+}
+
